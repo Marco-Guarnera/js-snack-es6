b4391d9a7bf71b5098a6475fa8be0fa0d16f6e86
Marco-Guarnera <Darkaquon93@gmail.com>
2024-08-29 15:35:00 +0200

    first commit

---
+++ js/snack01.js
@@ -0,0 +1,7 @@
+// Snack Array Methods 01
+
+// Fase di preparazione
+// Fase di gestione eventi
+// Fase di raccolta dati
+// Fase di elaborazione
+// Fase di produzione
---
+++ js/snack02.js
@@ -0,0 +1,7 @@
+// Snack Array Methods 02
+
+// Fase di preparazione
+// Fase di gestione eventi
+// Fase di raccolta dati
+// Fase di elaborazione
+// Fase di produzione
